deccba6711344435fe3cb2644e753e0d1012ee95
Sarah Gerweck <sarah.a180@gmail.com>
2017-12-04 16:26:38 -0800

    Far simpler failure

--- Fail.scala
+++ Fail.scala
@@ -1,14 +1,9 @@
 import language.reflectiveCalls
 
-import scala.util.parsing._
-import combinator._
-import lexical._
-
-trait LexerUtil extends Scanners {
-  def reflectiveLongest[A <: { def chars: String }](constants: Iterable[A]): Parser[A] = {
-    longest(constants, { a: A => a.chars })
+object LexerUtil {
+  def reflectiveLongest[A <: { def chars: String }](data: A): Unit = {
+    val fn = { a: A => a.chars }
+    ()
   }
-
-  def longest[A](constants: Iterable[A], chars: A => String): Parser[A]
 }
 
--- build.sbt
+++ build.sbt
@@ -1,5 +1,1 @@
 enablePlugins(ScalaJSPlugin)
-
-libraryDependencies += "org.scala-lang.modules" %%% "scala-parser-combinators" % "1.0.5"
-
-scalaJSLinkerConfig ~= { _.withModuleKind(ModuleKind.CommonJSModule) }
